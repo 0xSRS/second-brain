af2ce782ef0c01ffb229045e6cc3a95da480a533
Suryansh <suryanshrajsingh10405@gmail.com>
2026-03-31 23:20:28 +0530

    Implemented Button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import Button from "./components/Button"
+import LoginIcon from "./icons/LoginIcon"
+import SignUpIcon from "./icons/SignUpIcon"
 
 
 const App = () => {
   return (
     <div>
-      App
+      <Button text="Login" varient="primary" startIcon={<LoginIcon/>}/>
+      <Button text="Sign Up" varient="secondary" startIcon={<SignUpIcon/>}/>
     </div>
   )
 }
--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,8 +1,21 @@
+import type { ReactElement } from "react"
 
+interface ButtonProps{
+  varient:"primary" | "secondary",
+  text:string,
+  startIcon?:ReactElement
+}
 
-const Button = () => {
+const varientStyles = {
+  primary: "text-white font-semibold bg-primary hover:bg-primary-h",
+  secondary: "text-white bg-secondary hover:bg-secondary-h"
+}
+
+const defaultSyles = "rounded-xl p-2 p-1 min-w-25 transition-all duration-400 active:scale-90 flex items-center justify-center gap-2 "
+
+const Button = (props:ButtonProps) => {
   return (
-    <button ></button>
+    <button className={`${varientStyles[props.varient]} ${defaultSyles}`}>{(props.startIcon)? props.startIcon : null}{props.text}</button>
   )
 }
 
